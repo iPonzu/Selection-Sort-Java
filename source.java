d603d0f34bc35efa4d74f534afa1a503c024c528
iPonzu <joao_ignaciocastro1603@hotmail.com>
2022-08-30 21:10:15 -0300

    first commit

---
+++ source.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class source {
+    public static void main(String[] args) {
+         
+    System.out.println("Atividade com Selection Sort");
+        String numeros = new String();
+        File entrada = new File ("./Java/listaNumero.txt");
+        
+
+
+        
+       
+
+
+
+
+        
+    
+    
+}
+}
